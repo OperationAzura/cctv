e44ea4da94701614583b945618e969207f10d8f8
amazingfly <derekstich@gmail.com>
2020-09-10 00:43:24 -0400

    displayClass

---
+++ displayClass.py
@@ -0,0 +1,110 @@
+import cv2
+from imageStreamClass import ImageStreamClass
+import queue
+import multiprocessing
+import sys
+
+class Display(multiprocessing.Process):
+    def __init__(self, title, width, height, imgQ,  scale=100):
+        multiprocessing.Process.__init__(self)
+        self.imgQ = imgQ
+        self.origionalImage = None
+        self.croppedImage = None
+        self.image = None
+        self.title = title
+        self.scale = scale
+        self.width = width
+        self.height = height
+        self.centerX = int(width / 2)
+        self.centerY = int(height / 2)
+        self.radiusX = int(scale*self.centerX/100)
+        self.radiusY = int(scale*self.centerY/100)
+        self.minX = self.centerX - self.radiusX
+        self.maxX = self.centerX + self.radiusX
+        self.minY = self.centerY - self.radiusY
+        self.maxY = self.centerY + self.radiusY
+
+    #run overides the threading run function and
+    #displays the 
+    def run(self):
+        while True:
+            #get image from queue
+            try:
+                self.origionalImage = self.imgQ.get(False)
+            except: # no new image
+                pass
+            try:
+                self.ApplyMag()
+            except:
+                print('fram error?')
+            self.image = cv2.rotate(self.image, cv2.ROTATE_180 )
+            self.DisplayImageWindow()
+
+    def SetMagnification(self, scaleChange):
+        self.scale += scaleChange
+        print('scale: ', self.scale)
+        self.radiusX = int(self.scale * self.centerX / 100)
+        self.radiusY = int(self.scale * self.centerY / 100)
+        self.minX = self.centerX - self.radiusX
+        self.maxX = self.centerX + self.radiusX
+        self.minY = self.centerY - self.radiusY
+        self.maxY = self.centerY + self.radiusY
+        
+    def ApplyMag(self):
+        self.croppedImage = self.origionalImage[self.minY : self.maxY, self.minX : self.maxX]
+        self.image = cv2.resize(self.croppedImage, (self.screenWidth, self.screenHeight)) 
+    
+    #DisplayImageWindow displays the image 
+    def DisplayImageWindow(self):
+        cv2.namedWindow(self.title, cv2.WINDOW_NORMAL)
+        #cv2.moveWindow(window_name, screen.x - 1, screen.y - 1)
+        cv2.setWindowProperty(self.title, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        
+        cv2.imshow(self.title, self.image)
+        k = cv2.waitKey(50)
+        if k == 27:         # wait for ESC key to exit
+            print('esc key hit')
+            cv2.destroyAllWindows()
+            self.piCamera.close()
+            sys.exit(0)
+        elif k == 82 :
+            self.SetMagnification( 5)
+            print('up arrorw: ', k)
+        elif k == 84:
+            self.SetMagnification( -5)
+            print('dow arrow: ', k)
+
+    #SetTitle sets the title
+    def SetTitle(self, title):
+        self.title = title
+
+    #GetTitle gets the title
+    def GetTitle(self, ):
+        return self.title
+
+    #SetWidth sets the width
+    def SetWidth(self, width):
+        self.width = width
+
+    #__del__ destuctor to release resources
+    def __del__(self):
+        pass
+if __name__ == "__main__":
+    import screeninfo
+    from imageStreamClass import ImageStream
+
+    screen = screeninfo.get_monitors()[0]
+    sWidth, sHeight = screen.width, screen.height
+    width = 640
+    height = 480
+    title = 'frame'
+    imgQ = multiprocessing.Queue
+    print('running from display class file')
+    x = ImageStream(title=title, width=width, height=height, imgQ=imgQ)
+    y = Display(title, sWidth, sHeight, imgQ)
+    print('ImageStream Object created')
+    x.start()
+    y.start()
+    print('after StartCapture')
+    x.join()
+    y.join()
